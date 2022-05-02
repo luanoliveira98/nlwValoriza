3cdada0b08b0d435baadaa1fd32b222bdaa1ecbc
Luan <luanoliveira17@hotmail.com>
2022-05-02 13:05:21 -0300

    Aula 04 - Criando lógica de autenticação de usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 import { CreateUserController } from "./controllers/CreateUserController";
 import { CreateTagController } from "./controllers/CreateTagController";
-import { ensureAdmin } from "./middlewares/ensureAdmin";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
 router.post("/users", createUserController.handle)
+
+const authenticateUserController = new AuthenticateUserController();
+router.post("/login", authenticateUserController.handle);
 
 const createTagController = new CreateTagController();
 router.post("/tags", ensureAdmin, createTagController.handle)
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,29 @@
+import { getCustomRepository } from "typeorm";
+import { UserRepositories } from "../repositories/UsersRepositories";
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({ email, password } : IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UserRepositories);
+
+    const user = await usersRepositories.findOne({ email });
+    if(!user) throw new Error("Email/Password incorrect!");
+
+    const passwordMatch = await compare(password, user.password);
+    if(!passwordMatch) throw new Error("Email/Password incorrect!");
+
+    const token = sign({ email: user.email }, "b07d9600a51cfaa00f5dcfce6696ded4", { subject: user.id, expiresIn: "1d"});
+    return token;
+  }
+
+}
+
+export { AuthenticateUserService }
